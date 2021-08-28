e9c3ebff2d6e181d24538619b42243d9e130a2d2
Sean Corfield <sean@corfield.org>
2021-08-27 22:52:02 -0700

    Switch to build-clj lib

--- build.clj
+++ build.clj
@@ -9,63 +9,20 @@
   For more information, run:
 
   clojure -A:deps -T:build help/doc"
-  (:require [clojure.tools.build.api :as b]
-            [clojure.tools.deps.alpha :as t]
-            [deps-deploy.deps-deploy :as dd]))
+  (:require #_[clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
 
 (def lib 'com.github.seancorfield/expectations)
 ;; we're not gold yet!
 (def version "2.0.0-alpha3" #_(format "2.0.%s" (b/git-count-revs nil)))
-(def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn clean "Remove the target folder." [_]
-  (println "\nCleaning target...")
-  (b/delete {:path "target"}))
-
-(defn jar "Build the library JAR file." [_]
-  (println "\nWriting pom.xml...")
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :scm {:tag (str "v" version)}
-                :basis basis
-                :src-dirs ["src"]})
-  (println "Copying src...")
-  (b/copy-dir {:src-dirs ["src"]
-               :target-dir class-dir})
-  (println (str "Building jar " jar-file "..."))
-  (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
-
-(defn- run-task
-  [aliases]
-  (println "\nRunning task for:" aliases)
-  (let [basis    (b/create-basis {:aliases aliases})
-        combined (t/combine-aliases basis aliases)
-        cmds     (b/java-command {:basis     basis
-                                  :java-opts (:jvm-opts combined)
-                                  :main      'clojure.main
-                                  :main-args (cond-> (:main-opts combined)
-                                               (some #{:humane :cljs} aliases)
-                                               (conj "-e" ":negative"))})
-        {:keys [exit]} (b/process cmds)]
-    (when-not (zero? exit)
-      (throw (ex-info (str "Task failed for: " aliases) {})))))
-
-(defn run-tests
-  "Run regular tests.
-
-  Optionally specify :aliases:
-  [:1.9] -- test against Clojure 1.9 (the default)
-  [:1.10] -- test against Clojure 1.10.3
-  [:humane] -- test with HTO in the mix (and exclude :negative tests)
-  [:master] -- test against Clojure 1.11 master snapshot
-  [:cljs] -- test against ClojureScript"
+(defn run-tests "Run the tests."
   [{:keys [aliases] :as opts}]
-  (run-task (into [:test] aliases))
-  opts)
+  (-> opts
+      (cond->
+        (some #{:humane :cljs} aliases)
+        (assoc :main-opts ["-e" ":negative"]))
+      (bb/run-tests)))
 
 (defn ci
   "Run the CI pipeline of tests (and build the JAR).
@@ -73,14 +30,18 @@
   Specify :cljs true to run the ClojureScript tests as well."
   [opts]
   (-> opts
+      (assoc :lib lib :version version)
       (as-> opts
             (reduce (fn [opts alias]
-                      (run-tests (assoc opts :aliases (cond-> [alias]
-                                                        (not= :cljs alias)
-                                                        (conj :runner)))))
+                      (run-tests (assoc opts :aliases [alias])))
                     opts
                     (cond-> [:1.9 :1.10 :master :humane]
                       (:cljs opts)
                       (conj :cljs))))
-      (clean)
-      (jar)))
+      (bb/clean)
+      (bb/jar)))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
